b75312c7a2f9ce3f0067c9a1119714b0856db68a
Marco <165923504+Marco-Pedicillo@users.noreply.github.com>
2026-01-08 17:53:00 +0100

    Animations

--- app/src/main/java/com/camille/steply/pages/MainNavGraph.kt
+++ app/src/main/java/com/camille/steply/pages/MainNavGraph.kt
@@ -5,6 +5,13 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.camille.steply.pages.Routes
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.core.tween
+
+
 
 @Composable
 fun MainNavGraph() {
@@ -12,13 +19,15 @@ fun MainNavGraph() {
 
     NavHost(
         navController = navController,
-        startDestination = Routes.STEPS
+        startDestination = Routes.STEPS,
+        enterTransition = { fadeIn(animationSpec = tween(40)) },
+        exitTransition = { fadeOut(animationSpec = tween(40)) },
+        popEnterTransition = { fadeIn(animationSpec = tween(40)) },
+        popExitTransition = { fadeOut(animationSpec = tween(40)) }
+
     ) {
-        composable(Routes.STEPS) {
-            Home(navController = navController)
-        }
-        composable(Routes.ACTIVITY) {
-            ActivityScreen(navController = navController)
-        }
+        composable(Routes.STEPS) { Home(navController = navController) }
+        composable(Routes.ACTIVITY) { ActivityScreen(navController = navController) }
     }
+
 }
